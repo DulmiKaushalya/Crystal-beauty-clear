cd3c015de8ae0059dc2b9789b2838d0a127372d4
DulmiKaushalya <dulmikaushalya02@gmail.com>
2025-03-26 01:01:00 +0530

    add body-parser middleware and CRUD endpoints to the backend

--- crystal-beauty-clear-backend/index.js
+++ crystal-beauty-clear-backend/index.js
@@ -1,9 +1,41 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 let app = express();
 
-console.log('Hello, world!');
-console.log('This is the backend of the Crystal Beauty Clear project.');
+app.use(bodyParser.json());//middleware
+
+app.get('/',
+    (req,res)=>{
+        console.log(req.body)
+        console.log('Get request received');
+        res.json({message: 'Hello World'});
+    }
+);
+
+app.post('/',
+    (req,res)=>{
+        console.log(req.body)
+        console.log('Post request received');
+        res.json({message: 'Hello dul'});
+    }
+);
+
+app.put('/',
+    (req,res)=>{
+        console.log(req.body)
+        console.log('Put request received');
+        res.json({message: 'Hello Put'});
+    }
+);
+
+app.delete('/',
+    (req,res)=>{
+        console.log(req.body)
+        console.log('Delete request received');
+        res.json({message: 'Hello Delete'});
+    }
+);
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000');
