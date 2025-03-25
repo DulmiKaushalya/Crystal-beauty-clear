3aa51b090c4c521970dd7f03ef7376e8bfcef2ee
DulmiKaushalya <dulmikaushalya02@gmail.com>
2025-03-26 00:19:25 +0530

    add nodemon and semver modules with initial configurations

--- crystal-beauty-clear-backend/index.js
+++ crystal-beauty-clear-backend/index.js
@@ -1,2 +1,10 @@
+import express from 'express';
+
+let app = express();
+
 console.log('Hello, world!');
 console.log('This is the backend of the Crystal Beauty Clear project.');
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000');
+});
