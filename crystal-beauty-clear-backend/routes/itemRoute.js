6457cdf0662bb16bfb95b32c3b96dcd634d336b8
DulmiKaushalya <dulmikaushalya02@gmail.com>
2025-03-26 22:55:51 +0530

    fix: update item route to use ID parameter for searching items

--- crystal-beauty-clear-backend/routes/itemRoute.js
+++ crystal-beauty-clear-backend/routes/itemRoute.js
@@ -6,7 +6,7 @@ const itemRoute = express.Router();//empty router
 itemRoute.get("/",getItems)
 itemRoute.post("/",saveItem)
 itemRoute.get("/good",getGoodItems)//we need implement body part
-itemRoute.get("/:name",serachItem) // "/" after when we use name or id or etc.in here filtter that one,here no need to body part
+itemRoute.get("/:id",serachItem) // "/" after when we use name or id or etc.in here filtter that one,here no need to body part
 
 export default itemRoute;
 
